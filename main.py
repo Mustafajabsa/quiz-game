7dd7dfd2c95fb2281184e2b4e68e5b70e849ab00
Mustafajabsa <110934875+Mustafajabsa@users.noreply.github.com>
2023-11-17 10:55:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_banck = []
+
+for question in question_data:
+    question_banck.append(Question(question["text"], question["answer"]))
+
+quiz = QuizBrain(question_banck)
+
+while quiz.still_has_gestions():
+    quiz.next_guestion()
---
+++ question_model.py
@@ -0,0 +1,4 @@
+class Question():
+    def __init__(self, Q_text, Q_answer):
+        self.text = Q_text
+        self.answer = Q_answer
